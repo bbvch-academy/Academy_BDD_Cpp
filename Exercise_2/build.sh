43465f673ab1e5777d7c13fc64a98ba549ea0971
Michel Estermann <estermann.michel@gmail.com>
2017-01-02 20:20:41 +0100

    Removed unnecessary files from Exercise 2 project.

--- Exercise_2/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-WORKSPACE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${WORKSPACE_DIR}
-cmake --build build --target external_dependencies "$@"
-cmake --build build --target all "$@"
-
--- Exercise_2/configure.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-WORKSPACE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${WORKSPACE_DIR}
-mkdir -p build
-(cd build && cmake "$@" ..)
